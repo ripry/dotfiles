24e74eae76927219b2d0c6f3a47789ef2729baf7
Riku YAMAUCHI <ripry258@gmail.com>
2025-01-18 08:18:46 +0900

    Migrate to CachyOS

--- system/setup.sh
+++ system/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# OS: EndeavourOS
+# OS: CachyOS
 # DE: GNOME
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
@@ -19,15 +19,17 @@ symlink_configs() {
 }
 
 symlink_configs environment.d
-symlink_configs sysmemd/user
+symlink_configs systemd/user
 symlink_configs xremap
 symlink_configs alacritty
 symlink_configs tmux
 
 
-sudo systemctl enable --now bluetooth.service
+# Install AUR helper
+sudo pacman -S yay
 
 
+# Install thermal deamon
 yay -S thermald
 sudo systemctl enable --now thermald.service
 
@@ -35,8 +37,6 @@ sudo systemctl enable --now thermald.service
 # Setup browser connector for GNOME Shell extensions
 yay -S gnome-browser-connector
 
-# Fix broken dark theme of libadwaita
-yay -S xdg-desktop-portal-gnome
 
 # Setup key remapper
 # ref: https://extensions.gnome.org/extension/5060/xremap/
@@ -49,17 +49,15 @@ echo "uinput" | sudo tee -a /etc/modules-load.d/uinput.conf
 echo 'KERNEL=="uinput", GROUP="input", MODE="0660"' | sudo tee /etc/udev/rules.d/99-input.rules
 
 # Autostart xremap with systemd
-sysmemd --user enable --now xremap.service
+systemctl --user enable --now xremap.service
 
 
 # Setup input method
-yay -S fcitx5 fcitx5-gtk fcitx5-mozc
+yay -S fcitx5 fcitx5-mozc fcitx5-config-qt
 
 # Autostart fcitx5 with XDG Autostart
 sudo mkdir -p /etc/xdg/autostart
 sudo cp /usr/share/applications/org.fcitx.Fcitx5.desktop /etc/xdg/autostart
-
-# ref: https://extensions.gnome.org/extension/261/kimpanel/
 
 
 # Install fonts for Japanese
@@ -75,24 +73,42 @@ yay -S visual-studio-code-bin cursor-bin
 
 
 # Setup terminal
-yay -S alacritty tmux tmux-pluin-manager
-
-
-# Install Windows app runner
-yay -S wine winetricks wine-gecko wine-mono lib32-gnutls
+yay -S alacritty tmux tmux-plugin-manager
 
 
 # Setup docker
 yay -S docker docker-compose docker-buildx
 # ref: https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user
 sudo groupadd docker; sudo usermod -aG docker $USER; newgrp docker
-systemctl enable --now docker.service
+sudo systemctl enable --now docker.service
 
 
 # Setup zsh as default
 yay -S zsh
 chsh -s $(which zsh)
 
+
 # Setup Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+
+# Install GNOME Shell extensions manager
+yay -S extension-manager gnome-extensions-cli
+
+# Install input method integration
+gext install kimpanel@kde.org
+
+# Customize appearance
+gext install \
+  dash-to-dock@micxgx.gmail.com \
+  dash-to-panel@jderose9.github.com \
+  blur-my-shell@aunetx \
+  Vitals@CoreCoding.com \
+  appindicatorsupport@rgcjonas.gmail.com
+
+# Add useful functions
+gext install \
+  caffeine@patapon.info \
+  Battery-Health-Charging@maniacx.github.com \
+  another-window-session-manager@gmail.com
